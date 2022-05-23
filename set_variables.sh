8a61e1b5e0eb5f27edceab61c5eb8f4340615432
João Bonfim <jpa@lionx.com.br>
2022-05-23 15:25:38 -0300

    fix: fixed fission deployment and HTTP route

--- set_variables.sh
+++ set_variables.sh
@@ -1,4 +1,4 @@
-export FISSION_ENVIRONMENT_NAME="env-get-client-activity"
-export FISSION_FUNCTION_NAME="fn-get-client-activity"
-export FISSION_FUNCTION_ROUTE="/get_client_activity_type"
+export FISSION_ENVIRONMENT_NAME="env-get-activity-type"
+export FISSION_FUNCTION_NAME="fn-get-activity-type"
+export FISSION_FUNCTION_ROUTE="/enum/activity_type"
 export FISSION_TCP_PORT="8000"
